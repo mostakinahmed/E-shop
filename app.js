f773b8940e125ac45c65ae8994c5d4998776220c
mostakinahmed <mostakin4556@gmail.com>
2025-08-06 11:36:25 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/admin", (req, res) => {
+  res.render("admin");
+});
+
+app.get("/product", (req, res) => {
+  res.render("product");
+});
+
+app.get("/order", (req, res) => {
+  res.render("order");
+});
+
+app.get("/addNew", (req, res) => {
+  res.render("addNew");
+});
+
+app.get("/vieworder", (req, res) => {
+  res.render("vieworder");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running.....");
+});
